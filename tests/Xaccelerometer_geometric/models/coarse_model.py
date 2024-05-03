1f97b4cd59543669cff29078d6fdb678972642f5
LucaCaroselli <luca.caroselli@mail.polimi.it>
2024-05-03 09:37:23 +0200

    coarse and MDA identification, coarse adjustment

---
+++ tests/Xaccelerometer_geometric/models/coarse_model.py
@@ -0,0 +1,211 @@
+import numpy as np
+import os 
+
+w0 = np.loadtxt('./models/w0_opt.txt')
+w1 = np.loadtxt('./models/w1_opt.txt')
+w2 = np.loadtxt('./models/w2_opt.txt')
+
+def compute_stiffness(w, th , E , l1 , l2 , oe): 
+    """
+    Inputs:
+    w - Beam width
+    th - Beam thickness 
+    E - Young's modulus of the beam material
+    l1 - Length of the longer beams
+    l2 - Length of the shorter beams
+    oe - Overetch affecting the effective width of the beam
+
+     Output:
+     kTotal - Total stiffness of the folded beam structure
+    """
+    effectiveWidth = w - 2*oe
+    J = (1/12) * th * effectiveWidth**3
+    #Stiffness of individual beams based on their length
+    k1 = 12 * E * J / (l1**3 * 4)
+    k2 = 12 * E * J / (l2**3 * 2)
+    kTotal = 2 / (1/k1 + 1/k2)
+    
+    return kTotal
+
+def fringing_coeff( G, W ,L):
+    return (1+ G/np.pi/W + G/np.pi/W*np.log(2*np.pi*W/G))*(1+ G/np.pi/L + G/np.pi/L*np.log(2*np.pi*L/G))
+    
+
+def coarse_model(params):
+    """
+    - Inputs:
+    - parameters[0] : Overetch 
+    - parameters[1] : Offset
+    - parameters[2] : Thickness
+
+    - Output:
+    - C : An array containing the computed difference of capacitance 
+    """
+    # Time parameters
+    t0 = 0                    # Initial time.
+    tf = 0.0015-1e-5          # Final time.
+    dt =1e-5                  # Time step size.
+
+    # Stiffness parameters
+    l1 = 221.4*1e-6              # Lenght of the longer beam 
+    l2 = 110*1e-6            # Length of the shorted beam
+    E  = 160*1e9               # Young Modulus 
+    w  = 2.8*1e-6              # Width 
+
+    # Force parameters 
+    phi = lambda t: 0.9*(1-np.cos(2*np.pi*2500*t)) if t < 2/2500 else 0 # Voltage in the right electrodes
+    s   = 101*1e-6             # param for the surface 
+    dp  =1.2*1e-6                # Distance from the plates with Overetch and Offset = 0 
+
+    # Mass parameters
+    rho = 2320                 # Density of the mass.
+    A   = 84*1e-9              # Area of the component
+
+    # Damping parameters 
+    alpha = 31400            # Damping coefficient alpha.
+    beta  =  0               # Damping coeff beta 
+
+    # Input Parameters 
+    oe = params[0]*1e-6 
+    of = params[1]*1e-6
+    th = params[2]*1e-6 # ricorda di rimettere 6
+   
+    eps0 = 8.854*1e-12       # Dielectric permittivity constant
+    eps1 = 1.000             # Relative dielectric permittivity of air.
+
+    # Compute the distance between the faces of electrodes and the sensor
+    dl = dp+2*oe+of
+    dr = dp+2*oe-of
+    # Compute the surface of the electrode 
+    S = th * (s - 2*oe) * 10  # multiply by ten since we have 10 condensators
+    
+    # Initial conditions
+    u0 = 0  # Initial displacement
+    v0 = 0  # Initial velocity
+    N = int((tf - t0) / dt)
+
+    # Initialization
+    u = np.zeros((N+1))  # displacement
+    v = np.zeros((N+1))  # velocity
+    C = np.zeros((N+1))  # capacitance
+    u[0] = u0
+    v[0] = v0
+    C[0] = eps1*eps0*S*(1/(dr)*fringing_coeff( dr, s-2*oe ,th) - 1/(dl)*fringing_coeff( dl, s-2*oe ,th))
+
+    # Compute the stiffness
+    k = compute_stiffness(w, th , E , l1 , l2 , oe) 
+   
+    # Compute mass
+    m = rho * A * th
+    # Compute Damping 
+    damp = alpha* m + beta* k
+
+    # Precompute phi values to avoid redundant computation
+    k1 =  0.5 * eps0 * eps1 * S
+    k2 = eps1*eps0*S
+    F_values = np.array([phi(n * dt) for n in range(N)])**2 * k1
+
+    # Time-stepping loop using Forward Euler scheme
+    for n in range(N) :
+        u_n = u[n]
+        v_n = v[n]
+        u_new = u_n + dt * v_n
+        # Compute the value of the input voltage at time t = n*dt 
+        F = F_values[n]/((dr-u_n)**2)
+        v[n+1] = v_n + dt * ( F - damp*v_n - k*u_n )/m
+        # Compute the difference of capacitance
+        C[n+1] = k2*(1/(dr-u_new)*fringing_coeff( dr-u_new, s-2*oe ,th) - 1/(dl+u_new)*fringing_coeff( dl+u_new, s-2*oe ,th))
+        u[n+1] = u_new
+
+    return C*1e15*1.02
+
+def coarse_model_adj(params):
+    """
+    - Inputs:
+    - parameters[0] : Overetch 
+    - parameters[1] : Offset
+    - parameters[2] : Thickness
+
+    - Output:
+    - C : An array containing the computed difference of capacitance 
+    """
+    # Time parameters
+    t0 = 0                    # Initial time.
+    tf = 0.0015-1e-5          # Final time.
+    dt =1e-5                  # Time step size.
+
+    # Stiffness parameters
+    l1 = 221.4*1e-6              # Lenght of the longer beam 
+    l2 = 110*1e-6            # Length of the shorted beam
+    E  = 160*1e9               # Young Modulus 
+    w  = 2.8*1e-6              # Width 
+
+    # Force parameters 
+    phi = lambda t: 0.9*(1-np.cos(2*np.pi*2500*t)) if t < 2/2500 else 0 # Voltage in the right electrodes
+    s   = 101*1e-6             # param for the surface 
+    dp  =1.2*1e-6                # Distance from the plates with Overetch and Offset = 0 
+
+    # Mass parameters
+    rho = 2320                 # Density of the mass.
+    A   = 84*1e-9              # Area of the component
+
+    # Damping parameters 
+    alpha = 31440            # Damping coefficient alpha. 31400
+    beta  =  0               # Damping coeff beta 
+
+    # Input Parameters 
+    oe = params[0]*1e-6
+    of = params[1]*1e-6
+    th = params[2]*1e-6 # ricorda di rimettere 6
+   
+    eps0 = 8.854*1e-12       # Dielectric permittivity constant
+    eps1 = 1.000             # Relative dielectric permittivity of air.
+
+    # Compute the distance between the faces of electrodes and the sensor
+    dl = dp+2*oe+of
+    dr = dp+2*oe-of
+    # Compute the surface of the electrode 
+    S = th * (s - 2*oe) * 10  # multiply by ten since we have 10 condensators
+    
+    # Initial conditions
+    u0 = 0  # Initial displacement
+    v0 = 0  # Initial velocity
+    N = int((tf - t0) / dt)
+
+    # Initialization
+    u = np.zeros((N+1))  # displacement
+    v = np.zeros((N+1))  # velocity
+    C = np.zeros((N+1))  # capacitance
+    u[0] = u0
+    v[0] = v0
+    C[0] = eps1*eps0*S*(1/(dr)*fringing_coeff( dr, s-2*oe ,th) - 1/(dl)*fringing_coeff( dl, s-2*oe ,th))
+
+    # Compute the stiffness
+    k = compute_stiffness(w, th , E , l1 , l2 , oe) 
+   
+    # Compute mass
+    m = rho * A * th
+    # Compute Damping 
+    damp = alpha* m + beta* k
+
+    # Precompute phi values to avoid redundant computation
+    k1 =  0.5 * eps0 * eps1 * S
+    k2 = eps1*eps0*S
+    F_values = np.array([phi(n * dt) for n in range(N)])**2 * k1
+
+    # Time-stepping loop using Forward Euler scheme
+    for n in range(N) :
+        u_n = u[n]
+        v_n = v[n]
+        u_new = u_n + dt * v_n
+        # Compute the value of the input voltage at time t = n*dt 
+        F = F_values[n]/((dr-u_n)**2)
+        v[n+1] = v_n + dt * ( F - damp*v_n - k*u_n )/m
+        # Compute the difference of capacitance
+        C[n+1] = k2*(1/(dr-u_new)*fringing_coeff( dr-u_new, s-2*oe ,th) - 1/(dl+u_new)*fringing_coeff( dl+u_new, s-2*oe ,th))
+        u[n+1] = u_new
+    
+    # Adjusting phase 
+    C = C*1.02*1e15 + w0*params[0] + w1*params[1] + w2*params[2]
+
+    return C
